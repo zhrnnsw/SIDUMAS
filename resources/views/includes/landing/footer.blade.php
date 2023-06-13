71b0815bc80d12fd52fa4e6efac4b80828db68c0
Zahra Annisa Wahono <zahraannisawahono08@gmail.com>
2023-06-13 23:09:46 +0700

    Landing Page

---
+++ resources/views/includes/landing/footer.blade.php
@@ -0,0 +1,7 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+        <div class="text-center">
+            © 2023 | By <strong>TI-2F Kelompok 5</strong>
+        </div>
+  </footer><!-- End Footer -->
