1d0354c927389e2f3ce37bb96f77683960a7c7ab
andhitogalih <andhitognc@gmail.com>
2023-06-15 19:08:15 +0700

    edit miggration,controller,dan blade

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('nik')->unique();
+            $table->string('username')->unique();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('roles')->default(0);
+            /* Users: 0=>User, 1=>Admin */
             $table->rememberToken();
             $table->timestamps();
         });
