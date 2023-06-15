9f58b4cea53c1d776221c5e0ac1fa3a54e082300
andhitogalih <andhitognc@gmail.com>
2023-06-15 19:37:42 +0700

    Update login controller dan Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            'nik' => '250404',
+            'username'=>'adminn',
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'phone' => '085335249308',
+            'password' => Hash::make('admin'),
+            'roles' => '0',
+        ]);
     }
 }
