29c87eb759dd5452383bdb7e320f4658e542a921
yasminenavishaa <2141720047@student.polinema.ac.id>
2023-06-17 15:45:41 +0700

    Menambahkan Controller admin, pengaduan, user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,120 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pengaduan;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf as PDF;
 
 class AdminController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        return view('pages.admin.home');
+        return view('pages.admin.home',[
+            'pengaduan' => Pengaduan::all()->count(),
+            'user' => User::where('roles','=', 'user')->count(),
+            'admin' => User::where('roles', '=', 'admin')->count(),
+            'pending' => Pengaduan::where('status', 'Belum di Proses')->count(),
+            'process' => Pengaduan::where('status', 'Sedang di Proses')->count(),
+            'success' => Pengaduan::where('status', 'Selesai')->count(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function masyarakat()
+    {
+        $data = User::all()->where('roles','=', 0);
+
+        return view('pages.admin.masyarakat', [
+            'data' => $data
+        ]);
+    }
+
+    public function laporan() {
+
+        $pengaduan = Pengaduan::all();
+
+        return view('pages.admin.laporan',[
+            'pengaduan' => $pengaduan
+        ]);
+    }
+
+    public function cetak_laporan() {
+
+        $pengaduan = Pengaduan::all();
+
+        $pdf = PDF::loadview('pages.admin.pdf_laporan',['pengaduan' => $pengaduan]);
+        return ($pdf->download('laporan.pdf'));
+        
     }
 }
---
+++ app/Http/Controllers/PengaduanController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pengaduan;
+use App\Http\Requests\StorePengaduanRequest;
+use App\Http\Requests\UpdatePengaduanRequest;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
+use Barryvdh\DomPDF\Facade\Pdf as PDF;
+
+class PengaduanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pengaduan = Pengaduan::all();//->where('status','!=','Belum di Proses');
+       
+        return view('pages.admin.pengaduan',['pengaduan' => $pengaduan,
+    ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePengaduanRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePengaduanRequest $request)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pengaduan  $pengaduan
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $pengaduan = Pengaduan::find($id);
+        $user = User::find($pengaduan->user_id);
+        return view('pages.admin.detail',[
+            'pengaduan' => $pengaduan,
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pengaduan  $pengaduan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $pengaduan = Pengaduan::find($id);
+        $user = User::find($pengaduan->user_id);
+        return view(('pages.admin.edit'),['pengaduan'=>$pengaduan,'user' => $user],compact('pengaduan','user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePengaduanRequest  $request
+     * @param  \App\Models\Pengaduan  $pengaduan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $pengaduan = Pengaduan::all();
+        DB::table('pengaduan')->where('id', $id)->update([
+            'status'=> $request->status,
+            'bidang_id' => $request->bidang_id
+        ]);
+        $pengaduans = Pengaduan::where('id',$id)->first();
+        /*$pengaduan -> id = $request->get('id');
+        $pengaduan -> description = $request->get('description');
+        $pengaduan -> image = $request->get('image');
+        $pengaduan -> status = $request->get('status');
+        $pengaduan -> user_id = $request->get('user_id');
+        $pengaduan -> tingkatan_id = $request->get('tingkatan_id');*/
+        $pengaduans -> status = $request->get('status');
+        $pengaduans -> bidang_id = $request->get('bidang_id');
+        $pengaduans -> save();
+        Alert::success('Berhasil', 'Pengaduan berhasil ditanggapi');
+        return view('pages.admin.pengaduan',['pengaduan' => $pengaduan,]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pengaduan  $pengaduan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $pengaduan = Pengaduan::find($id);
+        $pengaduan->delete();
+
+        Alert::success('Berhasil', 'Pengaduan telah di hapus');
+        return redirect('admin/pengaduan');
+    }
+
+    /**
+     * Summary of cetak_pengaduan
+     * @param mixed $id
+     * @return \Illuminate\Http\Response
+     */
+    public function cetak_pengaduan($id) {
+
+        $pengaduan = Pengaduan::find($id);
+
+        $pdf = PDF::loadview('pages.admin.pdf_pengaduan', compact('pengaduan'))->setPaper('a4');
+        return $pdf->download('pengaduan.pdf');
+    }
+}
